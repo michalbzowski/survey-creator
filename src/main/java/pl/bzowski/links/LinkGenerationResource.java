961bc78a4d088fcc42c68578cd2eebd24f093a49
michalbzowski <michal.bzowski@gmail.com>
2025-08-23 03:25:13 +0200

    Info if you already send response and logger

--- src/main/java/pl/bzowski/links/LinkGenerationResource.java
+++ src/main/java/pl/bzowski/links/LinkGenerationResource.java
@@ -9,12 +9,16 @@ import pl.bzowski.surveys.Survey;
 import pl.bzowski.persons.Person;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 @Path("/api/v1/links")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class LinkGenerationResource {
+
+    Logger logger = Logger.getLogger(LinkGenerationResource.class.getName());
 
     @GET
     public List<PersonSurveyLink> listAllLinks() {
@@ -25,23 +29,30 @@ public class LinkGenerationResource {
     @Path("/{surveyId}")
     @Transactional
     public Response generateLinks(@PathParam("surveyId") UUID surveyId) {
+        logger.info("Generate links for " + surveyId.toString());
         Survey survey = Survey.findById(surveyId);
         if (survey == null) {
+            logger.info("Survey is null");
             return Response.status(Response.Status.NOT_FOUND).entity("Zapytanie nie istnieje").build();
         }
 
         List<Person> persons = Person.listAll();
+        logger.info("Persons found: " + (long) persons.size());
         for (Person person : persons) {
             // Sprawdź, czy link już istnieje
-            boolean exists = PersonSurveyLink.find("person = ?1 and survey = ?2", person, survey).firstResultOptional().isPresent();
+            Optional<PersonSurveyLink> personSurveyLink = PersonSurveyLink.find("person = ?1 and survey = ?2", person, survey).firstResultOptional();
+
+            boolean exists = personSurveyLink.isPresent();
             if (!exists) {
+                logger.info("Link doesn't exists for: " + person.email + " - " + surveyId);
                 PersonSurveyLink link = new PersonSurveyLink(person, survey);
                 link.persist();
+            } else {
+                logger.info("Person " + person.email + " has already link!");
             }
         }
 
         // Przekierowanie do widoku szczegółów zapytania
         return Response.seeOther(UriBuilder.fromPath("/web/surveys/{id}/details").build(surveyId)).build();
-
     }
 }
