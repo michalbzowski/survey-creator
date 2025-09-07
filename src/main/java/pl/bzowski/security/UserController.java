880bd432ac3c80127372c9afdc79e058ce75a8dd
michalbzowski <michal.bzowski@gmail.com>
2025-09-07 04:51:53 +0200

    Logout with better url

--- src/main/java/pl/bzowski/security/UserController.java
+++ src/main/java/pl/bzowski/security/UserController.java
@@ -4,11 +4,15 @@ import jakarta.inject.Singleton;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.NewCookie;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import pl.bzowski.base.RepositoryBase;
 
 @Path("/sec/")
 @Singleton
 public class UserController extends RepositoryBase {
+
+    @ConfigProperty(name = "app.host")
+    String appHost;
 
     @GET
     @Path("username")
@@ -23,7 +27,7 @@ public class UserController extends RepositoryBase {
         NewCookie removeQuarkusCredential = new NewCookie("quarkus-credential", "", "/", null, null, 0, false);
         NewCookie removeQSession = new NewCookie("q_session_chunk_1", "", "/", null, null, 0, false);
 
-        String keycloakLogoutUrl = "http://0.0.0.0:9999/realms/master/protocol/openid-connect/logout?redirect_uri=http%3A%2F%2Flocalhost:8080%2Flogged-out";
+        String keycloakLogoutUrl = appHost + "/realms/master/protocol/openid-connect/logout?redirect_uri=http%3A%2F%2Flocalhost:8080%2Flogged-out";
 
         Response.ResponseBuilder response = Response.seeOther(java.net.URI.create(keycloakLogoutUrl));
         response.cookie(removeQuarkusCredential);
