3fa37bf58c5ccd709f8c6c56192238f3726bc66d
michalbzowski <michal.bzowski@gmail.com>
2025-09-15 03:21:23 +0200

    email

--- src/main/java/pl/bzowski/email/EmailServiceImpl.java
+++ src/main/java/pl/bzowski/email/EmailServiceImpl.java
@@ -1,7 +1,8 @@
 package pl.bzowski.email;
 
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.reactive.ReactiveMailer;
 import io.smallrye.mutiny.Uni;
-import io.vertx.ext.mail.MailMessage;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
@@ -9,16 +10,10 @@ import jakarta.inject.Inject;
 public class EmailServiceImpl implements EmailService {
 
     @Inject
-    io.vertx.mutiny.ext.mail.MailClient mailClient;
+    ReactiveMailer reactiveMailer;
 
     @Override
     public Uni<Void> sendEmail(String to, String subject, String body) {
-
-        MailMessage message = new MailMessage();
-        message.setFrom("potwierdzobecnosc.pl <noreplay@potwierdzobecnosc.pl>");
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setHtml(body);
-        return mailClient.sendMail(message).replaceWithVoid();
+        return reactiveMailer.send(Mail.withHtml(to, subject, body));
     }
 }
