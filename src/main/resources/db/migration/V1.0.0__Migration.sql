af5fb46148d9f45f1ad31fb91d57a4dbbf3d2c68
michalbzowski <michal.bzowski@gmail.com>
2025-10-20 04:00:50 +0200

    Migration reset - going to production

--- src/main/resources/db/migration/V1.0.0__Migration.sql
+++ src/main/resources/db/migration/V1.0.0__Migration.sql
@@ -1,69 +1,69 @@
---[Hibernate]
+;--[Hibernate]
     set client_min_messages = WARNING
---[Hibernate]
+;--[Hibernate]
     alter table if exists answers
        drop constraint if exists FKkw9v0ofgb1ct8g0u3ot00fknq
---[Hibernate]
+;--[Hibernate]
     alter table if exists answers
        drop constraint if exists FKrwcax3668umuvoopknh436pi5
---[Hibernate]
+;--[Hibernate]
     alter table if exists answers
        drop constraint if exists FK64k8uhl7hl53ujygbqdkqjcxn
---[Hibernate]
+;--[Hibernate]
     alter table if exists events
        drop constraint if exists FKpp326w6hkksd9r1n696qa7pfv
---[Hibernate]
+;--[Hibernate]
     alter table if exists person_group
        drop constraint if exists FKb9l9rg2g7pqsr457prmhrcntk
---[Hibernate]
+;--[Hibernate]
     alter table if exists person_group
        drop constraint if exists FKiuq1ec2crebautk34jv5toaum
---[Hibernate]
+;--[Hibernate]
     alter table if exists persons
        drop constraint if exists FKeg2x5kl8cv8rglf8dpi5y7dcq
---[Hibernate]
+;--[Hibernate]
     alter table if exists team_event
        drop constraint if exists FKm4nc671p32vgtx4idl3sy1q6h
---[Hibernate]
+;--[Hibernate]
     alter table if exists team_event
        drop constraint if exists FKerrqsi3rchcilqnj9v4ngd1lu
---[Hibernate]
+;--[Hibernate]
     alter table if exists team_member
        drop constraint if exists FKijlvjelj7rsx6lq6gltu1pba3
---[Hibernate]
+;--[Hibernate]
     alter table if exists team_member
        drop constraint if exists FK53oqnmt6lsci5gh3g8yqw12i7
---[Hibernate]
+;--[Hibernate]
     drop table if exists answers cascade
---[Hibernate]
+;--[Hibernate]
     drop table if exists communication_agreement cascade
---[Hibernate]
+;--[Hibernate]
     drop table if exists communication_team_links cascade
---[Hibernate]
+;--[Hibernate]
     drop table if exists communications cascade
---[Hibernate]
+;--[Hibernate]
     drop table if exists events cascade
---[Hibernate]
+;--[Hibernate]
     drop table if exists groups cascade
---[Hibernate]
+;--[Hibernate]
     drop table if exists integrations cascade
---[Hibernate]
+;--[Hibernate]
     drop table if exists members cascade
---[Hibernate]
+;--[Hibernate]
     drop table if exists messenger_user_agreement cascade
---[Hibernate]
+;--[Hibernate]
     drop table if exists person_group cascade
---[Hibernate]
+;--[Hibernate]
     drop table if exists persons cascade
---[Hibernate]
+;--[Hibernate]
     drop table if exists tags cascade
---[Hibernate]
+;--[Hibernate]
     drop table if exists team_event cascade
---[Hibernate]
+;--[Hibernate]
     drop table if exists team_member cascade
---[Hibernate]
+;--[Hibernate]
     drop table if exists teams cascade
---[Hibernate]
+;--[Hibernate]
     create table answers (
         event_id uuid,
         id uuid not null,
@@ -72,7 +72,7 @@
         answerValue varchar(255) check (answerValue in ('TAK','NIE','ODPOWIEM_POZNIEJ')),
         primary key (id)
     )
---[Hibernate]
+;--[Hibernate]
     create table communication_agreement (
         agree boolean not null,
         id uuid not null,
@@ -83,14 +83,14 @@
         properties jsonb,
         primary key (id)
     )
---[Hibernate]
+;--[Hibernate]
     create table communication_team_links (
         communicationId uuid not null,
         id uuid not null,
         teamEntryId uuid not null,
         primary key (id)
     )
---[Hibernate]
+;--[Hibernate]
     create table communications (
         id uuid not null,
         personId uuid not null,
@@ -103,7 +103,7 @@
         properties jsonb,
         primary key (id)
     )
---[Hibernate]
+;--[Hibernate]
     create table events (
         localDateTime timestamp(6) not null,
         id uuid not null,
@@ -114,21 +114,21 @@
         name varchar(255) not null,
         primary key (id)
     )
---[Hibernate]
+;--[Hibernate]
     create table groups (
         id uuid not null,
         registered_user_id uuid not null,
         name varchar(255) not null,
         primary key (id)
     )
---[Hibernate]
+;--[Hibernate]
     create table integrations (
         id uuid not null,
         registered_user_id uuid not null,
         configuration jsonb,
         primary key (id)
     )
---[Hibernate]
+;--[Hibernate]
     create table members (
         teamAnswered boolean not null,
         id uuid not null,
@@ -141,7 +141,7 @@
         personTag varchar(255),
         primary key (id)
     )
---[Hibernate]
+;--[Hibernate]
     create table messenger_user_agreement (
         agree boolean not null,
         id uuid not null,
@@ -151,13 +151,13 @@
         psid varchar(255) not null,
         primary key (id)
     )
---[Hibernate]
+;--[Hibernate]
     create table person_group (
         group_id uuid not null,
         person_id uuid not null,
         primary key (group_id, person_id)
     )
---[Hibernate]
+;--[Hibernate]
     create table persons (
         id uuid not null,
         registered_user_id uuid not null,
@@ -168,85 +168,85 @@
         primary key (id),
         unique (email, registered_user_id)
     )
---[Hibernate]
+;--[Hibernate]
     create table tags (
         id uuid not null,
         registered_user_id uuid not null,
         name varchar(255) not null,
         primary key (id)
     )
---[Hibernate]
+;--[Hibernate]
     create table team_event (
         event_id uuid not null unique,
         team_id uuid not null
     )
---[Hibernate]
+;--[Hibernate]
     create table team_member (
         member_id uuid not null,
         team_id uuid not null,
         primary key (team_id)
     )
---[Hibernate]
+;--[Hibernate]
     comment on column team_member.team_id is
         ''
---[Hibernate]
+;--[Hibernate]
     create table teams (
         id uuid not null,
         registered_user_id uuid not null,
         name varchar(255),
         primary key (id)
     )
---[Hibernate]
+;--[Hibernate]
     alter table if exists answers
        add constraint FKkw9v0ofgb1ct8g0u3ot00fknq
        foreign key (event_id)
        references events
---[Hibernate]
+;--[Hibernate]
     alter table if exists answers
        add constraint FKrwcax3668umuvoopknh436pi5
        foreign key (member_id)
        references members
---[Hibernate]
+;--[Hibernate]
     alter table if exists answers
        add constraint FK64k8uhl7hl53ujygbqdkqjcxn
        foreign key (team_id)
        references teams
---[Hibernate]
+;--[Hibernate]
     alter table if exists events
        add constraint FKpp326w6hkksd9r1n696qa7pfv
        foreign key (team_id)
        references teams
---[Hibernate]
+;--[Hibernate]
     alter table if exists person_group
        add constraint FKb9l9rg2g7pqsr457prmhrcntk
        foreign key (group_id)
        references groups
---[Hibernate]
+;--[Hibernate]
     alter table if exists person_group
        add constraint FKiuq1ec2crebautk34jv5toaum
        foreign key (person_id)
        references persons
---[Hibernate]
+;--[Hibernate]
     alter table if exists persons
        add constraint FKeg2x5kl8cv8rglf8dpi5y7dcq
        foreign key (tag_id)
        references tags
---[Hibernate]
+;--[Hibernate]
     alter table if exists team_event
        add constraint FKm4nc671p32vgtx4idl3sy1q6h
        foreign key (event_id)
        references events
---[Hibernate]
+;--[Hibernate]
     alter table if exists team_event
        add constraint FKerrqsi3rchcilqnj9v4ngd1lu
        foreign key (team_id)
        references teams
---[Hibernate]
+;--[Hibernate]
     alter table if exists team_member
        add constraint FKijlvjelj7rsx6lq6gltu1pba3
        foreign key (member_id)
        references teams
---[Hibernate]
+;--[Hibernate]
     alter table if exists team_member
        add constraint FK53oqnmt6lsci5gh3g8yqw12i7
        foreign key (team_id)
